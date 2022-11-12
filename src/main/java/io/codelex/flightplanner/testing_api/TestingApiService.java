340e9a91cdbfc910959ce2a21830314bd67ad3a1
yapijs <daviszarins90@gmail.com>
2022-11-12 22:57:37 +0200

    admin authentification and clear route

---
+++ src/main/java/io/codelex/flightplanner/testing_api/TestingApiService.java
@@ -0,0 +1,13 @@
+package io.codelex.flightplanner.testing_api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestingApiService {
+
+    public TestingApiService() {
+    }
+
+    public void clearData() {
+    }
+}
